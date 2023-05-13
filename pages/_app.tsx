eb435a9cfe7d920d57fe822f3dfb215ffe41c86d
Sean Finegan <sean.j.finegan@gmail.com>
2023-05-13 09:04:28 -0700

    fixed session error with typescript.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,16 @@ import { SessionProvider } from 'next-auth/react';
 import { AppProps } from 'next/app';
 
 
+interface Props extends AppProps {
+  pageProps: {
+    session: any;
+  }
+}
 
-const App = ({ Component, pageProps }: AppProps) => {
-  return (
-    <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
-};
+const App = ({ Component, pageProps }: Props) => (
+  <SessionProvider session={pageProps.session}>
+    <Component {...pageProps} />
+  </SessionProvider>
+);
 
 export default App;
